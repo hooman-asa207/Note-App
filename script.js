bcf13691f124fc631645d8db639eab4315cad8f3
Hooman Asadian <asadianhuman@gmail.com>
2024-02-12 19:45:18 +0330

    added update note features and finish the project

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const days = ["Saturday", "Sunday", "Monday", "Tuesday", 'Wednesday', 'Thursday', 'Friday']
+const days = ["Sunday", "Monday", "Tuesday", 'Wednesday', 'Thursday', 'Friday', "Saturday"]
 
 const $ = document
 const addNoteBox = $.querySelector(".add-box")
@@ -8,12 +8,12 @@ const popUpBox = $.querySelector('.popup-box')
 const addOrUpdateNote = popUpBox.querySelector("button")
 const titleInput = popUpBox.querySelector(".title input")
 const desInput = popUpBox.querySelector(".description textarea")
+const inputAlert = $.querySelector(".alert")
 
 let notesArray = []
 let noteTitle, noteDes
 
 let myTime = new Date()
-
 let year = myTime.getFullYear()
 let dayNum = myTime.getDay()
 let dayOfMounth = myTime.getDate()
@@ -21,43 +21,74 @@ let monthsNum = myTime.getMonth()
 
 let isUpdate = false
 
-function openPopUp() {
-    popUpBox.classList.add("show")
-    popUpBox.querySelector('.uil-times').addEventListener("click", () => { popUpBox.classList.remove("show") })
+let mainNoteidForUpdate
 
-    if (isUpdate === false) {
-        popUpBox.querySelector(".content header p").innerHTML = 'Add new note'
-        popUpBox.querySelector(".content form button").innerHTML = 'Add new note'
-    } else {
-        popUpBox.querySelector(".content header p").innerHTML = 'Update note'
-        popUpBox.querySelector(".content form button").innerHTML = 'Update note'
-    }
+function openPopUp() {
+  inputAlert.classList.remove("show")
+  popUpBox.classList.add("show")
+  popUpBox.querySelector('.uil-times').addEventListener("click", () => {
+    popUpBox.classList.remove("show")
+    isUpdate = false
+  })
+
+  if (isUpdate === false) {
+    popUpBox.querySelector(".content header p").innerHTML = 'Add new note'
+    popUpBox.querySelector(".content form button").innerHTML = 'Add new note'
+    emptyInputs()
+  } else {
+    popUpBox.querySelector(".content header p").innerHTML = 'Update note'
+    popUpBox.querySelector(".content form button").innerHTML = 'Update note'
+
+    let mainNoteId = notesArray.findIndex(note => note.id == mainNoteidForUpdate)
+    titleInput.value = notesArray[mainNoteId].title
+    desInput.value = notesArray[mainNoteId].description
+  }
 }
 
 function submitNote() {
-    let newNote = {
+  if (titleInput.value !== '') {
+    inputAlert.classList.remove("show")
+
+    if (isUpdate === false) {
+      let newNote = {
         id: notesArray.length + 1,
         title: titleInput.value,
         description: desInput.value
+      }
+      notesArray.push(newNote)
+      setLocalStorage(notesArray)
+      generateNote(notesArray)
+
+    } else {
+
+      let mainNoteId = notesArray.findIndex(note => note.id == mainNoteidForUpdate)
+      notesArray[mainNoteId].title = titleInput.value
+      notesArray[mainNoteId].description = desInput.value
+
+      setLocalStorage(notesArray)
+      generateNote(notesArray)
+
+      isUpdate = false
     }
-    notesArray.push(newNote)
-    setLocalStorage(notesArray)
-    generateNote(notesArray)
+
     popUpBox.classList.remove("show")
-    titleInput.value = ''
-    desInput.value = ''
+    emptyInputs()
+
+  } else {
+    inputAlert.classList.add("show")
+  }
 }
 
 function setLocalStorage(notesArray) {
-    localStorage.setItem('notes', JSON.stringify(notesArray))
+  localStorage.setItem('notes', JSON.stringify(notesArray))
 }
 
 function generateNote(notesArray) {
-    noteWrapper.querySelectorAll('li').forEach(li => li.remove())
+  noteWrapper.querySelectorAll('li').forEach(li => li.remove())
 
-    notesArray.forEach(note => {
-        noteWrapper.insertAdjacentHTML("beforeend",
-            `<li class="note">
+  notesArray.forEach(note => {
+    noteWrapper.insertAdjacentHTML("beforeend",
+      `<li class="note">
       <div class="details">
         <p>${note.title}</p>
         <span>${note.description}</span>
@@ -67,40 +98,53 @@ function generateNote(notesArray) {
         <div class="settings">
           <i class="uil uil-ellipsis-h" onclick="openSetting(this)"></i>
           <ul class="menu">
-            <li onclick="openUpdate(${note.id})>
+            <li onclick="update(${note.id})">
               <i class="uil uil-pen"></i>Edit
             </li>
-            <li onclick="openDelete(${note.id})">
+            <li onclick="Delete(${note.id})">
               <i class="uil uil-trash"></i>Delete
             </li>
           </ul>
         </div>
       </div>
     </li>`)
-    })
+  })
 }
 
 function getLocalStorage() {
-    notesArray = localStorageTodos = JSON.parse(localStorage.getItem('notes')) || []
-    generateNote(notesArray)
+  notesArray = localStorageTodos = JSON.parse(localStorage.getItem('notes')) || []
+  generateNote(notesArray)
 }
 
-function openSetting(el) { el.parentNode.classList.add('show') }
-
-function openDelete(id) {
-    let notes = localStorageTodos = JSON.parse(localStorage.getItem('notes'))
-    notesArray = notes
-
-    let mainNote = notesArray.findIndex(note => {
-        return note.id === id
-    })
-
-    notesArray.splice(mainNote, 1)
-    setLocalStorage(notesArray)
-    generateNote(notesArray)
-
+let menuElem
+function openSetting(el) {
+  menuElem = el
+  menuElem.parentNode.classList.toggle('show')
+  addNoteBox.addEventListener("click", () => menuElem.parentNode.classList.remove('show'))
 }
 
+function Delete(id) {
+  let notes = localStorageTodos = JSON.parse(localStorage.getItem('notes'))
+  notesArray = notes
+
+  let mainNote = notesArray.findIndex(note => { return note.id === id })
+
+  notesArray.splice(mainNote, 1)
+  setLocalStorage(notesArray)
+  generateNote(notesArray)
+}
+
+function update(id) {
+  menuElem.parentNode.classList.remove('show')
+  mainNoteidForUpdate = id
+  isUpdate = true
+  openPopUp()
+}
+
+function emptyInputs() {
+  titleInput.value = ''
+  desInput.value = ''
+}
 
 addNoteBox.addEventListener("click", openPopUp)
 addOrUpdateNote.addEventListener("click", submitNote)
